b6b3b720811a036bb737fe1d54b5cec733794380
davidagitlen <davidagitlen@gmail.com>
2019-08-20 16:40:04 -0600

    Complete initial fetch for person data

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component }from 'react';
 import './App.css';
+// import Fetcher from '../Fetcher/Fetcher';
 
 class App extends Component {
   constructor() {
@@ -16,6 +17,7 @@ class App extends Component {
     fetch('https://swapi.co/api/people/')
     .then(response => response.json())
     .then(data => this.fetchPeople(data.results))
+    .then(data => this.fetchSpecies(data))
     .then(people => this.setState({people: people}))
     .catch(error => console.log(error));
   }
@@ -25,31 +27,53 @@ class App extends Component {
       return fetch(person.homeworld)
         .then(response => response.json())
         .then(data => ({
-          
           homeworld: data.name,
           population: data.population,
-          name: person.name
+          name: person.name,
+          species: person.species
          }))
         .catch(error => console.log(error));
     });
     return Promise.all(promises);
   };
 
+  fetchSpecies = (people) => {
+    const promises = people.map(person => {
+      return fetch(person.species)
+        .then(response => response.json())
+        .then(data => ({
+          ...person,
+          language: data.language,
+          species: data.name
+        }))
+        .catch(error => console.log(error));
+    });
+    return Promise.all(promises);
+  };
+
   render() {
+    if (this.state.people.length) {
     let peopleList = this.state.people.map(person => {
-      return <>
+      return (<>
         <p>{person.name}</p>
         <p>{person.homeworld}</p>
         <p>{person.species}</p>
         <p>{person.language}</p>
         <p>{person.population}</p>
-      </>;
-    });
+      </>)}
+      );
+      return (
+        <main>
+          {peopleList}
+        </main>
+      );
+    } else {
     return (
       <main>
-        {peopleList}
+        <p>Blah</p>
       </main>
-    )
+      )
+    }
   }
 }
 
