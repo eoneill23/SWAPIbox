a31503cb3a2e497c08cb896d1eb401294d7f94c2
Eric O'Neill <oneill.eric23@gmail.com>
2019-08-20 15:20:55 -0600

    Got person fetch to work

--- src/App/App.js
+++ src/App/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component }from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      vehicles: [],
+      people: [],
+      planets: [],
+      films: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://swapi.co/api/people/')
+    .then(response => response.json())
+    .then(data => this.fetchPeople(data.results))
+    .then(people => this.setState({people: people}))
+    .catch(error => console.log(error));
+  }
+
+  fetchPeople = (people) => {
+    const promises = people.map(person => {
+      return fetch(person.homeworld)
+        .then(response => response.json())
+        .then(data => ({
+          
+          homeworld: data.name,
+          population: data.population,
+          name: person.name
+         }))
+        .catch(error => console.log(error));
+    });
+    return Promise.all(promises);
+  };
+
+  render() {
+    let peopleList = this.state.people.map(person => {
+      return <>
+        <p>{person.name}</p>
+        <p>{person.homeworld}</p>
+        <p>{person.species}</p>
+        <p>{person.language}</p>
+        <p>{person.population}</p>
+      </>;
+    });
+    return (
+      <main>
+        {peopleList}
+      </main>
+    )
+  }
 }
 
 export default App;
