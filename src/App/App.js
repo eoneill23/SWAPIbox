312602929f279aa8f04b1a48b3c7b9d85d521a0d
davidagitlen <davidagitlen@gmail.com>
2019-08-20 19:03:25 -0600

    Finish planet handling function

--- src/App/App.js
+++ src/App/App.js
@@ -15,11 +15,28 @@ class App extends Component {
 
   componentDidMount() {
     fetch('https://swapi.co/api/people/')
-    .then(response => response.json())
-    .then(data => this.fetchPeople(data.results))
-    .then(data => this.fetchSpecies(data))
-    .then(people => this.setState({people: people}))
-    .catch(error => console.log(error));
+      .then(response => response.json())
+      .then(data => this.fetchPeople(data.results))
+      .then(data => this.fetchSpecies(data))
+      .then(people => this.setState({people: people}))
+      .catch(error => console.log(error));
+      setTimeout(() => {
+        console.log('state', this.state)
+      }, 3000);
+
+    fetch('https://swapi.co/api/planets/')
+      .then(response => response.json())
+      // .then(data => console.log(data.results));
+      // .then(data => data.results.map(datum => ({
+      //   name: datum.name,
+      //   terrain: datum.terrain,
+      //   population: datum.population,
+      //   climate: datum.climate,
+      //   residents: datum.residents
+      // })))
+      .then(data => this.fetchResidents(data.results))
+      .then(data => console.log('after function', data))
+      .catch(error => console.log(error))
   }
 
   fetchPeople = (people) => {
@@ -49,6 +66,27 @@ class App extends Component {
         .catch(error => console.log(error));
     });
     return Promise.all(promises);
+  };
+
+  fetchResidents = (planets) => {
+    
+    const planetsArray = planets.map(planet => {
+      let nameArray = [];
+      planet.residents.map(resident => {
+        return fetch(resident)
+        .then(response => response.json())
+        .then(data => nameArray.push(data.name))
+        .catch(error => console.log(error))
+      });
+      return {
+        name: planet.name,
+        terrain: planet.terrain,
+        population: planet.population,
+        climate: planet.climate,
+        residents: nameArray
+      }
+    });
+    return planetsArray
   };
 
   render() {
