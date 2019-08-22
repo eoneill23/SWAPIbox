266614dcd09cea2f6b93499b11174802680d8716
davidagitlen <davidagitlen@gmail.com>
2019-08-22 17:32:40 -0600

    Add fill colors and style navlinks in Header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import xWing from '../images/x-wing.svg';
+// import nonChunkyDeathStar from '../images/non-chunky-deathstar.svg';
+import chunkyDeathStar from '../images/chunky-death-star.svg'
+import bobaFett from '../images/boba-fett.svg';
 import './Header.css';
 
 const Header = () => {
 
   return (
     <header className='Header'>
-      <NavLink to='/people' className='nav'>People</NavLink>
-      <NavLink to='/planets' className='nav'><img />Planets</NavLink>
+      <NavLink to='/people' className='nav'><img src={bobaFett}/>People</NavLink>
+      <NavLink to='/planets' className='nav'><img src={chunkyDeathStar} className='deathstar'/>Planets</NavLink>
       <NavLink to='/vehicles' className='nav'><img src={xWing} />Vehicles</NavLink>
     </header>
   )
