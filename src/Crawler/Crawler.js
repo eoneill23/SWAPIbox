b9852f202a4b20703ec0d3c119ad618875f899d1
Eric O'Neill <oneill.eric23@gmail.com>
2019-08-22 18:41:02 -0600

    Get crawler to work

--- src/Crawler/Crawler.js
+++ src/Crawler/Crawler.js
@@ -4,9 +4,11 @@ import './Crawler.css'
 const Crawler = ({openingText}) => {
   
   return(
-    <div className='Crawler'>
-      <p>{openingText}</p>
-    </div>
+    <section className='Crawler'>
+      <div className='crawl-text'>
+        <p>{openingText}</p>
+      </div>
+    </section>
   )
 }
 
