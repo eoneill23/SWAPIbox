bdeed82243e8b1d46f1b3266c38668b4cdebd596
davidagitlen <davidagitlen@gmail.com>
2019-08-23 11:17:22 -0600

    Include initial tests for routes from app

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App'
+import { mount, shallow } from 'enzyme';
+import { MemoryRouter } from 'react-router';
+import App from './App';
+import Crawler from '../Crawler/Crawler';
+import Cards from '../Cards/Cards';
 
 describe('App', () => {
   let wrapper;
@@ -10,5 +13,30 @@ describe('App', () => {
   })
   it('should render without crashing', () => {
     expect(wrapper.length).toEqual(1)
+  });
+
+  describe('Route', () => {
+
+    it('should route to the homepage with the Crawler displayed by default', () => {
+
+      const wrapper = mount(
+        <MemoryRouter initialEntries={['/']}>
+          <App />
+        </MemoryRouter>
+      );
+
+      expect(wrapper.find(Crawler)).toHaveLength(1);
+    });
+
+    it('should route to the people page when the people link is clicked', () => {
+      const wrapper = mount(
+        <MemoryRouter initialEntries={['/people']}>
+          <App />
+        </MemoryRouter>
+      );
+
+      expect(wrapper.find(Cards)).toHaveLength(1);
+    })
+
   });
 });
