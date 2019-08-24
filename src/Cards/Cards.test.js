e80047e1e1acfec6d7ba4db28dca8f817cb832f3
Eric O'Neill <oneill.eric23@gmail.com>
2019-08-24 16:30:15 -0600

    Build out mock data in Cards.test.js

--- src/Cards/Cards.test.js
+++ src/Cards/Cards.test.js
@@ -3,11 +3,27 @@ import { shallow } from 'enzyme';
 import Cards from './Cards'
 
 describe('Cards', () => {
-  let wrapper, mockPerson
+  let wrapper, mockPeople, mockPlanets, mockVehicles, mockFavorites
 
   beforeEach(() => {
     wrapper = shallow(<Cards data={[]}/>)
-    mockPerson = { name: 'David', homeworld: 'Kamino', species: 'wookie', population: 20, favorite: 'false' };
+    mockPeople = [
+      { name: 'David', homeworld: 'Kamino', species: 'wookie', population: 20, favorite: 'false' },
+      { name: 'Eric', homeworld: 'Tatooine', species: 'human', population: 42, favorite: 'false' }
+    ];
+    mockPlanets = [
+      { name: 'Tatooine', terrain: 'grasslands, mountains', population: 200, climate: 'temperate', residents: ['Travis', 'Robbie'], favorite: 'false' },
+      { name: 'Coruscant', terrain: 'desert', population: 42, climate: 'temperate', residents: ['Steve', 'Paul'], favorite: 'false' }
+    ]
+    mockVehicles = [
+      { name: 'Toyota Corolla', model: 'sedan', class: 'wheeled', passengers: 4, favorite: 'false' }, 
+      { name: 'Sand Crawler', model: 'Digger Crawler', class: 'wheeled', passengers: 30, favorite: 'false'}
+    ]
+    mockFavorites = [
+      { name: 'David', homeworld: 'Kamino', species: 'wookie', population: 20, favorite: 'true' },
+      { name: 'Eric', homeworld: 'Tatooine', species: 'human', population: 42, favorite: 'true' },
+      { name: 'Toyota Corolla', model: 'sedan', class: 'wheeled', passengers: 4, favorite: 'true' }
+    ];
   });
 
   it('should render without crashing', () => {
