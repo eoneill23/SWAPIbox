4ff724f79b96fc60d001bfa028c124a35729030d
Eric O'Neill <oneill.eric23@gmail.com>
2019-08-25 12:15:45 -0600

    Get SVGs aligned in header, attempt active link styling, and add propTypes to Header and Cards Components

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import xWing from '../images/x-wing.svg';
 import chunkyDeathStar from '../images/chunky-death-star.svg';
 import bobaFett from '../images/boba-fett.svg';
@@ -8,20 +9,23 @@ import nonChunkyFavoriteLightsaber from '../images/non-chunky-favorite-lightsabe
 import './Header.css';
 
 const Header = ({data}) => {
-  console.log('header data', data)
   const favorites = data;
   return (
     <header className='Header'>
       <h1>SWAPIbox</h1>
       <div className='Header-links'>
-        <NavLink to='/' className='nav'><img src={nonChunkyHut} />Home</NavLink>
+        <NavLink to='/' className='nav'><img className='small-header-img' src={nonChunkyHut} />Home</NavLink>
         <NavLink to='/people' className='nav'><img src={bobaFett} />People</NavLink>
         <NavLink to='/planets' className='nav'><img src={chunkyDeathStar} className='deathstar'/>Planets</NavLink>
         <NavLink to='/vehicles' className='nav'><img src={xWing} />Vehicles</NavLink>
-        <NavLink to='/favorites' className='nav'><img src={nonChunkyFavoriteLightsaber} />Favorites{favorites.length}</NavLink>
+        <NavLink to='/favorites' className='nav'><img className='small-header-img smaller' src={nonChunkyFavoriteLightsaber} />Favorites{favorites.length}</NavLink>
       </div>
     </header>
   )
+}
+
+Header.propTypes = {
+  data: PropTypes.array
 }
 
 export default Header;
