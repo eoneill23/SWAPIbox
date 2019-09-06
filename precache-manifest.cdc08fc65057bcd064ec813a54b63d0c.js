dbf13780dda6e385641647c9eb951501029bcd98
davidagitlen <davidagitlen@gmail.com>
2019-09-06 11:46:57 -0600

    Updates

---
+++ precache-manifest.cdc08fc65057bcd064ec813a54b63d0c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bba90841b5bcd33b1f9186f7b2eb356",
+    "url": "/SWAPIbox/index.html"
+  },
+  {
+    "revision": "408fb60aa7a49097ea32",
+    "url": "/SWAPIbox/static/css/main.7461dd89.chunk.css"
+  },
+  {
+    "revision": "9b0afc92a0407e1f7dc3",
+    "url": "/SWAPIbox/static/js/2.504d9f28.chunk.js"
+  },
+  {
+    "revision": "408fb60aa7a49097ea32",
+    "url": "/SWAPIbox/static/js/main.b9fdcb2f.chunk.js"
+  },
+  {
+    "revision": "0b8cb02843f82dd6a4d6",
+    "url": "/SWAPIbox/static/js/runtime~main.ba8de343.js"
+  },
+  {
+    "revision": "37c2957ef5e18e90cc27355cfe813ad5",
+    "url": "/SWAPIbox/static/media/boba-fett.37c2957e.svg"
+  },
+  {
+    "revision": "4cbf6d223c8bd0cf8a827d7ce17901c5",
+    "url": "/SWAPIbox/static/media/chunky-death-star.4cbf6d22.svg"
+  },
+  {
+    "revision": "8ed68362c21a664c9b2bd0c7cb032441",
+    "url": "/SWAPIbox/static/media/non-chunky-favorite-lightsaber.8ed68362.svg"
+  },
+  {
+    "revision": "e8780b99ec3255fcd9d128829f61efe4",
+    "url": "/SWAPIbox/static/media/non-chunky-hut.e8780b99.svg"
+  },
+  {
+    "revision": "25badfe2be2b70108b4edbe3ab2b2f60",
+    "url": "/SWAPIbox/static/media/x-wing.25badfe2.svg"
+  }
+]);
